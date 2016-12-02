361997eca2be3fa357f8d612683a05670b4272ff
Son Phuong <Son Phuong>
2016-12-02 16:55:25 -0600

    done coding

--- resources/views/pages/cvsc.blade.php
+++ resources/views/pages/cvsc.blade.php
@@ -1,17 +1,17 @@
 @extends('layout')
 @section('content')
-
 <div mc-messages></div>
 <section class="container" ng-controller="gameCtrl">
-	
-	<span ng-repeat="tool in tools track by $index" id="<% tool %>">
+	<div class="col-xs-2 no-margin" ng-repeat="tool in tools track by $index" id="<% tool %>">
 		<button disabled="true"><img src="../img/game/<% tool %>.png" class="icon"/></button>
-	</span>
-	<span ng-show="isPlayed"><a id="play-again" href='#' ng-click="resetCVSCGame()">Play Again</a></span>
-	<span ng-hide="isPlayed"><button type="button" class="btn btn-primary" ng-click="playCVSC()">Play</button></span>
-	<span ng-repeat="tool in tools track by $index" id="c<% tool %>">
+	</div>
+	<div class="col-xs-2 text-center">
+		<p>Com1 VS Com2</p>
+		<div ng-show="isPlayed"><a id="play-again" href='#' ng-click="resetCVSCGame()">Play Again</a></div>
+		<div ng-hide="isPlayed"><button type="button" class="btn btn-primary" ng-click="playCVSC()">Play</button></div>
+	</div>
+	<div class="col-xs-2 no-margin" ng-repeat="tool in tools track by $index" id="c<% tool %>">
 		<button disabled="true"><img src="../img/game/<% tool %>.png" class="icon"/></button>
-	</span>
+	</div>
 </section>
-
 @stop
--- resources/views/pages/game.blade.php
+++ resources/views/pages/game.blade.php
@@ -1,16 +1,17 @@
 @extends('layout')
 @section('content')
-
 <div mc-messages></div>
 <section class="container" ng-controller="gameCtrl">
 <h4>To play: click to choose a tool</h4>
-	<span ng-repeat="tool in tools track by $index" >
-		<button ng-disabled="isPlayed" ng-click="play($index)" id="<% tool %>"><img src="../img/game/<% tool %>.png" class="icon"/></button>
-	</span>
-	<span ng-show="isPlayed"><a id="play-again" href='#' ng-click="resetGame()">Play Again</a></span>
-	<span ng-repeat="tool in tools track by $index" ng-show="isPlayed" id="c<% tool %>">
+	<div class="col-xs-2 no-margin" ng-repeat="tool in tools track by $index" id="<% tool %>">
+		<button ng-disabled="isPlayed" ng-click="play($index)" ><img src="../img/game/<% tool %>.png" class="icon"/></button>
+	</div>
+	<div class="col-xs-2 text-center" ng-show="isPlayed">
+		<p>You VS Com</p>
+		<div><a id="play-again" href='#' ng-click="resetGame()">Play Again</a></div>	
+	</div>
+	<div class="col-xs-2 no-margin" ng-repeat="tool in tools track by $index" ng-show="isPlayed" id="c<% tool %>">
 		<button><img src="../img/game/<% tool %>.png" class="icon"/></button>
-	</span>
+	</div>
 </section>
-
 @stop
