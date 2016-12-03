fd1de2a56f1d8f817470c48242d0298f951351e9
Son Phuong <Son Phuong>
2016-12-03 17:02:28 -0600

    img res

--- resources/views/pages/game.blade.php
+++ resources/views/pages/game.blade.php
@@ -6,15 +6,15 @@
 	<div class="row">
 		<div class="center-block" ng-cloak>
 			<div class="col-xs-3 no-margin" ng-repeat="tool in tools track by $index" id="<% tool %>">
-				<button ng-disabled="isPlayed" ng-click="play($index)" class="no-style"><img ng-src="../img/game/<% tool %>.png" class="icon"/></button>
+				<button ng-disabled="isPlayed" ng-click="play($index)" class="no-style"><img ng-src="../img/game/<% tool %>.png" class="icon img-responsive"/></button>
 			</div>
 			<div class="col-xs-2 text-center" ng-show="isPlayed" >
 				<p>You VS Com</p>
 				<div id="rs"></div>
-				<div><a id="play-again" href='#' ng-click="resetGame()">Play Again</a></div>	
+				<div><a id="play-again" href='' ng-click="resetGame()">Play Again</a></div>	
 			</div>
 			<div class="col-xs-3 no-margin" ng-repeat="tool in tools track by $index" ng-show="isPlayed" id="c<% tool %>">
-				<button disabled="true" class="no-style"><img ng-src="../img/game/<% tool %>.png" class="icon"/></button>
+				<button disabled="true" class="no-style"><img ng-src="../img/game/<% tool %>.png" class="icon img-responsive"/></button>
 			</div>
 		</div>
 	</div>
