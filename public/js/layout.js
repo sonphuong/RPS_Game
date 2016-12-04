29618da155828bdaccdd0ae354742889f555250e
Son Phuong <Son Phuong>
2016-12-04 17:35:28 -0600

    window.location version work well

--- public/js/layout.js
+++ public/js/layout.js
@@ -1,24 +1,12 @@
-var app = angular.module('gameApp', ["ngRoute",'MessageCenterModule'], function($interpolateProvider,$locationProvider){
+var app = angular.module('gameApp', ["ngRoute",'MessageCenterModule'], function($interpolateProvider){
     //angularjs and laravel using the same symbol {{}} for display variable so need to change one of them to avoid conflict
     $interpolateProvider.startSymbol('<%');
     $interpolateProvider.endSymbol('%>');
-    $locationProvider.html5Mode({
-    	enabled: true,
-  		requireBase: false //if dont have this -> $location in HTML5 mode requires a <base> tag to be present
-    });//if dont set this $location.path() will return nothing
 });
-
-
-app.controller('layoutCtrl', ['$scope','$location', function($scope,$location){
+app.controller('layoutCtrl', ['$scope', function($scope){
     //set menu active
-    
-    $scope.path = $location.path();
-    $scope.$watch('path',function($path){
-	    $scope.isHomePage = ($path.indexOf("/")==1)?true:false;
-	    $scope.isGamePage = ($path.indexOf("game")==1)?true:false;
-	    $scope.isCVSCPage = ($path.indexOf("cvsc")==1)?true:false;
-	    console.log('location: '+$path);
-    });
-
+    $scope.isHomePage = (window.location.pathname.indexOf("/")==1)?true:false;
+    $scope.isGamePage = (window.location.pathname.indexOf("game")==1)?true:false;
+    $scope.isCVSCPage = (window.location.pathname.indexOf("cvsc")==1)?true:false;
 
 }]);
